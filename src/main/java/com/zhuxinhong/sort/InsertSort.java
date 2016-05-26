fc099a9759ef1b821ea157669ccfb56bbde4c25f
zhuxh <zhuxinhong@bilibili.com>
2016-05-26 11:15:24 +0800

    update maven plugin

---
+++ src/main/java/com/zhuxinhong/sort/InsertSort.java
@@ -0,0 +1,27 @@
+package com.zhuxinhong.sort;
+
+/**
+ * Created by zhuxh on 16/5/26.
+ */
+public class InsertSort {
+
+    private static final int a[] = {49, 38, 65, 97, 76, 13, 27, 49, 78, 34, 12, 64, 5, 4, 62, 99, 98, 54, 56, 17, 18, 23, 34, 15, 35, 25, 53, 51};
+
+    public static void main(String[] args) {
+        int temp = 0;
+
+        for (int i = 1; i < a.length; i++) {
+            int j = i - 1;
+            temp = a[i];
+            for (; j >= 0 && temp < a[j]; j--) {
+                a[j + 1] = a[j];
+            }
+            a[j + 1] = temp;
+        }
+
+        for (int i = 0; i < a.length; i++)
+            System.out.println(a[i]);
+
+    }
+}
+
